83f6e184174117e871377e4ac0386d37467f396b
Lee Sanghyeon <yongdamsh@gmail.com>
2017-02-01 13:04:51 +0900

    Correct `cartSelectors` import path

--- src/app/redux/ducks/cart/index.js
+++ src/app/redux/ducks/cart/index.js
@@ -1,6 +1,6 @@
 import { default as reducer } from "./reducers";
 
 export { default as cartOperations } from "./operations";
-export { default as cartSelectors } from "./operations";
+export { default as cartSelectors } from "./selectors";
 
 export default reducer;
