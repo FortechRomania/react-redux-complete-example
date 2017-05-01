f5fb1de0adf8ecd52ea47f617896fb5f03690a91
alexnm <alex.n.moldovan@gmail.com>
2017-05-01 18:00:40 +0300

    other dependencies

--- src/app/state/store.js
+++ src/app/state/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
-import createLogger from "redux-logger";
+import { createLogger } from "redux-logger";
 import * as reducers from "./ducks";
 import { apiService } from "./middlewares";
 
