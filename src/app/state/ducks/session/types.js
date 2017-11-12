fec63421c3bf08209377049ed7c8c108f3f836eb
Aaron P Roberts <31625704+uncommonAP@users.noreply.github.com>
2017-11-12 03:19:39 -0500

    I saw a typo in one of your session types :) (#18)

--- src/app/state/ducks/session/types.js
+++ src/app/state/ducks/session/types.js
@@ -2,5 +2,5 @@ export const LOGIN = "session/LOGIN";
 export const LOGIN_COMPLETED = "session/LOGIN_COMPLETED";
 export const LOGIN_FAILED = "session/LOGIN_FAILED";
 export const LOGOUT = "session/LOGOUT";
-export const INITIALIZE = "sesstion/INITIALIZE_SESSION";
+export const INITIALIZE = "session/INITIALIZE_SESSION";
 export const SET_REDIRECT_AFTER_LOGIN = "session/SET_REDIRECT_AFTER_LOGIN";
